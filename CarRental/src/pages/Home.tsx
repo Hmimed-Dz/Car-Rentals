0549008d54d31cbf121ee39bbcd58bf2f4bc271a
Ahmed <ah.kabouya@itsolutions.dz>
2023-11-29 20:35:41 +0100

    fixed css

--- CarRental/src/pages/Home.tsx
+++ CarRental/src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import SearchForm from "./components/form"
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function Home() {
-  const test = ""
-
   return (
     <div>
       <h1 style={{ color: "black", display: "flex", fontFamily: "sans-serif" }}>
         {" "}
-        Compare and find rental car deals to find the right one.
+        Compare rental car deals to find the right one.
       </h1>
       <SearchForm />
     </div>
--- CarRental/src/pages/components/form.tsx
+++ CarRental/src/pages/components/form.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react"
 import { Button, Form, Input } from "antd"
-import { CarFilled } from "@ant-design/icons"
-
+import {
+  CalculatorOutlined,
+  CarFilled,
+  CiCircleOutlined,
+} from "@ant-design/icons"
+import "../css/home.css"
 /* eslint-disable no-template-curly-in-string */
 const validateMessages = {
   required: "${label} is required!",
@@ -15,15 +18,14 @@ const validateMessages = {
 }
 /* eslint-enable no-template-curly-in-string */
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-explicit-any
 const onFinish = (values: any) => {
   console.log(values)
 }
 const layout = { labelCol: { span: 15 }, wrapperCol: { span: 5 } }
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function SearchForm() {
-  const name: any = ""
-  const [state, useName] = useState()
-
   return (
     <Form
       // {...layout}
@@ -36,26 +38,43 @@ function SearchForm() {
       <Form.Item name={["user", "name"]} rules={[{ required: true }]}>
         <Input
           placeholder="Car Mark"
-          prefix={<CarFilled className="site-form-item-icon" />}
+          prefix={
+            <CarFilled
+              className="site-form-item-icon"
+              style={{ height: "30px", width: "40px" }}
+            />
+          }
           style={{ width: "20rem", height: "3rem" }}
         />
       </Form.Item>
       <Form.Item name={["user", "email"]} rules={[{ type: "email" }]}>
         <Input
+          prefix={<CiCircleOutlined />}
           placeholder="unavailable choice"
           style={{ width: "20rem", height: "3rem" }}
         />
       </Form.Item>
       <Form.Item name={["user", "website"]}>
         <Input
+          prefix={<CalculatorOutlined />}
           placeholder="unavailable choice"
           style={{ width: "20rem", height: "3rem" }}
         />
       </Form.Item>
 
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
+        <Button
+          type="primary"
+          htmlType="submit"
+          style={{
+            width: "10rem",
+            height: "3rem",
+            fontSize: "19px",
+            color: "black",
+            background: "silver",
+          }}
+        >
+          Search
         </Button>
       </Form.Item>
     </Form>
